35459cf7b17f55917e84d848f88555fc924f979e
Adam Kafka <adkafka@gmail.com>
2016-04-27 00:17:58 -0400

    Merged test code so user can input which List to use

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"./Lists"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -37,7 +39,33 @@ func testList(list List, seed int, wg *sync.WaitGroup) {
 }
 
 func main() {
-	list := new(Lists.CGList)
+	//take in input to see which list to use
+	//TODO: change to command line input
+	fmt.Print("Enter 1 for coarse grain, 2 for lock free and 3 for lazy locking: ")
+	var inputstr string
+	_, err := fmt.Scanf("%s", &inputstr)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	input, e := strconv.Atoi(inputstr)
+	if e != nil {
+		fmt.Println(e)
+		os.Exit(1)
+	}
+	var list List
+
+	switch input {
+	case 1:
+		list = new(Lists.CGList)
+	case 2:
+		list = new(Lists.LFList)
+	case 3:
+		list = new(Lists.LazyList)
+	default:
+		fmt.Printf("improper input detected")
+		os.Exit(1)
+	}
 	list.Init()
 	var wg sync.WaitGroup
 	wg.Add(numthreads)
