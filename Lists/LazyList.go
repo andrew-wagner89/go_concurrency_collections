0a24c5d2ade578f3ba82271ee29ac3e361d0a0d0
Andrew Wagner <scalliwags65@gmail.com>
2016-05-12 23:45:13 -0400

    fixed infinite loop problem and added comments

--- Lists/LazyList.go
+++ Lists/LazyList.go
@@ -12,9 +12,9 @@ type NodeLL struct {
 	key    interface{}
 	val    interface{}
 	next   *NodeLL
-	marked bool
+	marked bool //marks whether the node is removed from the list
 	hash uint64
-	lock   *sync.Mutex
+	lock   *sync.Mutex //locks individual nodes
 }
 
 type LazyList struct {
@@ -22,6 +22,7 @@ type LazyList struct {
 	tail *NodeLL
 }
 
+//node constructor
 func make_nodeLL(key interface{},val interface{}, next *NodeLL) *NodeLL {
 	n := new(NodeLL)
 	n.key = key
@@ -34,14 +35,17 @@ func make_nodeLL(key interface{},val interface{}, next *NodeLL) *NodeLL {
 	return n
 }
 
+//initializes the list
 func (l *LazyList) Init() {
-	l.tail = make_nodeLL(0, nil, nil)
+	//sentinel key and val don't matter, just the hash value
+	l.tail = make_nodeLL(0, nil, nil) 
 	l.head = make_nodeLL(0, nil, l.tail)
-	l.head.hash = MIN_UINT64
-	l.tail.hash = MAX_UINT64
+	l.head.hash = MIN_UINT64 //head has minimum hash
+	l.tail.hash = MAX_UINT64 //tail has maximum hash
 
 }
 
+//prints out the list
 func (l *LazyList) Printlist() {
 
 	t := l.head
@@ -51,15 +55,17 @@ func (l *LazyList) Printlist() {
 	}
 }
 
+//validates whether the two nodes are still in the list and curr is the node after curr
 func validate(pred *NodeLL, curr *NodeLL) bool {
 	return !pred.marked && !curr.marked && pred.next == curr
 }
 
 //Member funcs for LazyList
 
+//inserts a key and val into a list, if the key is already in the list, the val is updated
 func (l *LazyList) Insert(key interface{}, val interface{}) bool {
-	var returnval bool
 
+	//hash the key for the search process
 	var keyHash uint64
 	hash32, _ := getHash(key)
 	keyHash = uint64(hash32)
@@ -73,59 +79,86 @@ func (l *LazyList) Insert(key interface{}, val interface{}) bool {
 			curr = curr.next
 		}
 
+		//lock the pred and curr since pred's next mey be changed, and currs val may be updated
 		pred.lock.Lock()
+		curr.lock.Lock()
 
-		if validate(pred, curr) {
-			if curr.hash == keyHash && curr.key == key {
-				curr.val = val
-				returnval = true
-			} else {
-				new_node := make_nodeLL(key, val, curr)
+		if validate(pred, curr) { //make sure pred and curr are still valid
+			if curr.hash == keyHash { 
+				updated := false
+				//handle hash collisions by checking all nodes with the search hash until the correct key is found
+				for curr.hash == keyHash { 
+					if curr.hash == keyHash && curr.key == key { //if the key is already in the list, update the val
+							curr.val = val
+							updated = true
+							break
+					}
+					//if the current node is not the correct node, look at the next node
+					pred.lock.Unlock()
+					pred = curr
+					curr = curr.next
+					curr.lock.Lock()
+					validate(pred, curr) //revalidate the nodes
+				}
+				if !updated { //if a val has not been updated, add a new node to the list
+					new_node := make_nodeLL(key, val, curr)
+					pred.next = new_node
+				}
+			} else { //if the current hash is not equal to the hash of the key, make a new node
+				new_node := make_nodeLL(key, val, curr) 
 				pred.next = new_node
-				returnval = true
 			}
+
 			pred.lock.Unlock()
+			curr.lock.Unlock()
 			break
 		}
 
+		//if the two nodes are not valid, restart the insert process
+		curr.lock.Unlock()
 		pred.lock.Unlock()
 	}
-	return returnval
+	return true
 }
 
+//get the val that corresponds to the key
+//returns two values in the form (val, keyFound)
+//keyFound is a boolean specifying if the key was found in the list
 func (l *LazyList) Get(key interface{}) (interface{}, bool) {
 
+	//hash the key for the search process
 	var keyHash uint64
 	hash32, _ := getHash(key)
 	keyHash = uint64(hash32)
 
+	//starting from the head, iterate through the list, looking for the hash
 	var curr *NodeLL = l.head
 
 	for curr.hash < keyHash {
 		curr = curr.next
 	}
 
+	//handle collisions with a while loop going through all nodes with the correct hash
 	for curr.hash == keyHash {
-		if curr.hash == keyHash && curr.key == key {
-			if !curr.marked {
-				return curr.val, true
-			}
+		if curr.hash == keyHash && curr.key == key && !curr.marked { //if the current node has the right key return it
+			return curr.val, true
 		}
 		curr = curr.next
 	}
 
-	return nil, false
+	return nil, false //if the correct key wasn't found return false
 }
 
+//removes the key from the list, returns whether the key was in the list or not
 func (l *LazyList) Remove(key interface{}) bool {
-	returnval := false
-	breakInfinite := false
 
+	//hash the value for searching purposes
 	var keyHash uint64
 	hash32, _ := getHash(key)
 	keyHash = uint64(hash32)
 
 	for {
+		//iterate through the list searching for the hash value
 		pred := l.head
 		curr := l.head.next
 
@@ -138,39 +171,40 @@ func (l *LazyList) Remove(key interface{}) bool {
 		curr.lock.Lock()
 
 
+		restart := false //restart holds whether an invalid node pair was found
 		if curr.hash == keyHash{
-			for curr.hash == keyHash {
+			for curr.hash == keyHash { //loop through all nodes with the correct hash
 				if validate(pred, curr) {
-					if curr.key == key{
+					if curr.key == key{ //if the key is in the list remove it and return true
 						curr.marked = true
 						pred.next = curr.next
-						returnval = true
-						breakInfinite = true
-						break
-					} else {
+						pred.lock.Unlock()
+						curr.lock.Unlock()
+						return true
+					} else { // if this node doesn't have the key, go to the next one
+						pred.lock.Unlock()
 						pred = curr
 						curr = curr.next
-						continue
+						curr.lock.Lock()
 					}
-				} else {
-					continue
+				} else{ //validate failed, stop the remove process
+					restart = true
+					break
 				}
-
 			}
-		} else {
-			returnval = false
 			pred.lock.Unlock()
 			curr.lock.Unlock()
-			break
-		}
 
-		pred.lock.Unlock()
-		curr.lock.Unlock()
-
-		if breakInfinite {
-			break
+			//if a validate didn't work, restart the remove process
+			if restart {	
+				continue
+			} else { //otherwise the hash is in the list, but the key isn't, return false
+				return false
+			}
+		} else { //if the hash value wasn't in the list return false
+			pred.lock.Unlock()
+			curr.lock.Unlock()
+			return false
 		}
 	}
-
-	return returnval
 }
